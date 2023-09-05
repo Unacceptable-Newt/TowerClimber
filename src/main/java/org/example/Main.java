42d38ec11dc91a2d7528b8f008c61be995972ad6
Yucheng Zhu <u7581147@anu.edu.au>
2023-09-05 23:05:38 +1000

    Test that GibLab PipeLine correctly fails failed tests.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public class Main {
+    /**
+     * @author Yucheng Zhu
+     *
+     * a test function used to test that GitLab classifies
+     * both passed and failed correctly
+     * @return an integer 1 that will be checked against tests.
+     */
+    public int testPipelineWorks() {
+        return 1;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- src/test/java/GitLabPipelineTest.java
+++ src/test/java/GitLabPipelineTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class GitLabPipelineTest {
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GitLabPipelineTest {
+    /**
+     * @author Yucheng Zhu
+     * Test that the GitLab Pipeline passes correct tests.
+     */
+    @Test
+    public void testGitLabPipelineWhenTestFails() {
+        Main main = new Main();
+
+        // This shall fail, because `2 != 1`
+        assertEquals(2, main.testPipelineWorks());
+    }
 }
