b67a55976d0c28c8fc2bc2dc343f3780c1b277f0
Austin Zerk <u6648099@anu.edu.au>
2023-09-27 18:07:46 +1000

    changed to have position retried from npc

--- src/main/java/org/example/IO/JsonSave.java
+++ src/main/java/org/example/IO/JsonSave.java
@@ -281,7 +281,7 @@ public class JsonSave {
                     Iterator<Position> NPCiter = NPCHashMap.keySet().iterator();
                     while (NPCiter.hasNext()) {
                         Position key = NPCiter.next();
-                        newMaze.addNPC(key, NPCHashMap.get(key));
+                        newMaze.addNPC(NPCHashMap.get(key));
                     }
 
                     newMaze.setEncodedWalls(wallArrayList);
