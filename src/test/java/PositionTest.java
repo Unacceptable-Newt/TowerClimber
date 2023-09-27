ec1771c242be11fe32cacb07bf13d2048aaab89f
Yucheng Zhu <u7581147@anu.edu.au>
2023-09-27 16:15:57 +1000

    Added more exit tests

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,22 @@
+import org.example.entity.Position;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Yucheng Zhu
+ * Test Position
+ */
+public class PositionTest {
+    /**
+     * @author Yucheng Zhu
+     * Test position setters
+     */
+    @Test
+    public void testSetter() {
+        Position position = new Position(1, 1);
+        position.setX(5);
+        position.setY(7);
+        Assertions.assertEquals(5, position.getX());
+        Assertions.assertEquals(5, position.getY());
+    }
+}
