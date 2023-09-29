7ee5d23a807bc4245e084427fbb54adc40345622
Yue Zhu <u7442130@anu.edu.au>
2023-09-29 17:09:37 +1000

    Delete EnemyDisplayTest.java

--- src/test/java/EnemyDisplayTest.java
+++
@@ -1,8 +0,0 @@
-public class EnemyDisplayTest {
-    /*
-     * @author Yue Zhu
-     * @This test is aimed to test whether the function, Display(the part for enemy), can work well
-     */
-
-    
-}
